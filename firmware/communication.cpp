03ab38b68ff86d235b0eb4bbab4d9f4204bca2d7
Christopher Peplin <chris.peplin@rhubarbtech.com>
2013-09-12 16:59:12 -0400

    Fix motor movement in response to commands.
    We were truncating the last ')' but that was required for the new
    parsing approach.

--- firmware/communication.cpp
+++ firmware/communication.cpp
@@ -10,15 +10,16 @@ void receiveCommands() {
     if(Serial.available()) {
         while(Serial.available() > 0){
             char c = Serial.read();
+            input += c;
             if(c == ')' || c =='>'){
                 parse_message(input);
                 input = "";
             } else if(c == 'u') {
                 motorStepUp();
+                input = "";
             } else if(c == 'd') {
                 motorStepDown();
-            } else {
-                input += c;
+                input = "";
             }
         }
     }
@@ -27,14 +28,21 @@ void receiveCommands() {
 void parse_message(String message) {
     if(message[0] == '(') {
         int digitVal = 10*(message[1]-'0') + (message[2]-'0');
-        //Update Digits
+        Serial.print("Setting display to: ");
+        Serial.print(message[1]);
+        Serial.println(message[2]);
         setDisplay(digitVal / 10, digitVal % 10);
     }
 
-    if(message[message.length()-1] == ')') {
+    Serial.print("message is: ");
+    Serial.println(message);
+    if(message[message.length() - 1] == ')') {
         double motorVal = 10*(message[message.length()-3]-'0') + (message[message.length()-2]-'0');
-        unsigned long motorPer = motorVal*1000/SPEED_SLOPE;
-        setMotorTarget(STEPS * motorPer / 100);
+        unsigned long motorPer = motorVal * 1000/SPEED_SLOPE;
+        unsigned long target = STEPS * motorPer / 100;
+        Serial.print("Setting motor target to: ");
+        Serial.println(target);
+        setMotorTarget(target);
     }
 
     if (message[0] == '<') {
